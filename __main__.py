03402bd895306a17b7024daeb86cf044b057b221
タジカラオ <103996576+Tajikarao@users.noreply.github.com>
2022-04-22 00:58:15 +0200

    [UPDATE] PIL to cv2 / numpy

--- __main__.py
+++ __main__.py
@@ -1,11 +1,7 @@
-import os
-import random
-import string
-
 import easyocr
 import numpy as np
 import requests
-from PIL import Image, ImageChops
+import cv2
 
 reader = easyocr.Reader(["en"], gpu=False, verbose=False)
 
@@ -26,34 +22,29 @@ class Solver(metaclass=Singleton):
             "captcha": "https://webresolver.nl/public/captcha/captcha.png"
         }
 
-        self.temp_name = self.get_random_string()
+    def prepare_captcha(self):
+        requests_picture = self.session.get(self.endpoints["captcha"], stream=True).raw
+        image = np.asarray(bytearray(requests_picture.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image
 
-    def prepare_captcha(self) -> None:
-        requests_picture = self.session.get(self.endpoints["captcha"], stream=True)
-        with open(f"{self.temp_name}.png", "wb") as f:
-            for chunk in requests_picture:
-                f.write(chunk)
+    @staticmethod
+    def prepare_background():
+        image = cv2.imread("background.png")
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image
 
-        return Image.open(f"{self.temp_name}.png")
+    @staticmethod
+    def remove_background(captcha, background):
+        subtract = cv2.subtract(background, captcha)
+        return np.invert(subtract)
 
-    def get_random_string(self) -> str:
-        return "".join(random.choice(string.ascii_letters) for _ in range(5))
-
-    def prepare_background(self):
-        return Image.open("background.png").convert("RGB")
-
-    def remove_background(self, captcha, background):
-        subtract = ImageChops.subtract(background, captcha)
-        mask1 = Image.eval(subtract, lambda a: 0 if a <= 24 else 255)
-        mask2 = mask1.convert("1")
-
-        blank = Image.eval(subtract, lambda a: 0)
-        new = Image.composite(background, blank, mask2)
-
-        return np.array(new)
-
-    def ocr(self, picure):
-        text = reader.readtext(picure, detail=0)[0]
+    @staticmethod
+    def ocr(picure):
+        text = reader.readtext(picure, detail=0)
+        if len(text) >= 1:
+            text = text[0]
         return "".join(text.split(" "))
 
 
@@ -65,15 +56,12 @@ def get_captcha():
     removed_brackground = solver.remove_background(captcha, background)
 
     captcha_text = solver.ocr(removed_brackground)
-    os.remove(f"{solver.temp_name}.png")
-
     return captcha_text
 
 
 if __name__ == "__main__":
-    wait_captcha = True
-    while wait_captcha:
+    while True:
         captcha = get_captcha()
         if len(captcha) == 4 and captcha.isdigit():
             print(captcha)
-            wait_captcha = False
+            exit(0)
